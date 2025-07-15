fe276e96ce2d28ff615b5c095d69d33a795d8e5d
BadhonDev <badhon@ebiw.com>
2025-07-16 02:15:38 +0600

    importMap

--- src/app/(payload)/admin/importmap.js
+++ src/app/(payload)/admin/importmap.js
@@ -2,6 +2,6 @@ import { Icon as Icon_da13d4a92d563854eec8d031f6ac5450 } from '../../../graphics
 import { Logo as Logo_b78bafd3a6af48632758eeee62fa2c8f } from '../../../graphics/Logo/index.jsx'
 
 export const importMap = {
-  "/graphics/Icon/index.jsx#Icon": Icon_da13d4a92d563854eec8d031f6ac5450,
-  "/graphics/Logo/index.jsx#Logo": Logo_b78bafd3a6af48632758eeee62fa2c8f
+  '/graphics/Icon/index.jsx#Icon': Icon_da13d4a92d563854eec8d031f6ac5450,
+  '/graphics/Logo/index.jsx#Logo': Logo_b78bafd3a6af48632758eeee62fa2c8f,
 }
